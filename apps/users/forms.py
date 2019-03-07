38c1d07af9c183be3e215e808018b5543c83712c
Hk4Fun <941222165nobody@gmail.com>
2019-03-07 09:01:35 +0800

    Upgrade to django2.1

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -4,7 +4,8 @@ __date__ = '2018/1/9 22:04'
 from django import forms
 from .models import User
 
+
 class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = User  # 选择要验证的表单model
-        fields = ['image'] # 选择要验证的字段
+        fields = ['image']  # 选择要验证的字段
